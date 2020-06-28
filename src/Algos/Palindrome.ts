c4a4c2076537426242d7f2b3d816c99e7ab4f0ca
Tawanda Kanyangarara <kliation@gmail.com>
2020-06-28 07:57:38 +0100

    Palindrome Comments

--- src/Algos/Palindrome.ts
+++ src/Algos/Palindrome.ts
@@ -8,17 +8,26 @@ export default class Fibonnaci {
      */
 
     static isPalindrome = (str: string): boolean => {
-        str = str.toLowerCase();
-        let characterArr = str.split('');
-        let validCharacters = 'abcdefghijklmnopqrstuvwxyz'.split('');
 
+        let characterArr = str.toLowerCase().split('');
+        let validCharacters = 'abcdefghijklmnopqrstuvwxyz'.split('');
         let lettersArr: string[] = [];
+
+        /**
+         * Checkis if the string characrter is a letter if true, then push to letters 
+         *  @returns {string} lowercase string Array
+         */
+
         characterArr.forEach(char => {
             if (validCharacters.indexOf(char) > -1) {
                 lettersArr.push(char);
             }
         });
 
+         /**
+         * Compares string against itself in reverse
+         * @returns {boolean}
+         */
         if (lettersArr.join('') === lettersArr.reverse().join('')) {
             return true;
         } else {
